4402912c44f5a34577553b73dee50cc5a1702c94
Kamil Stawik <kamil_stawik@op.pl>
2021-07-12 16:45:02 +0200

    styled-components added to Container modul

---
+++ src/Container/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    max-width: 600px;
+    margin: 10px auto;
+    padding: 10px;
+
+    @media (max-width: ${({ theme }) => (theme.breakPoints.mobile)}px) {
+        margin: 5px auto;
+        padding: 5px;
+    }
+`;
